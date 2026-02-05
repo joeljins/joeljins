f2c55668416f1209382647f0bb125f284d09d2c1
jj3249 <jj3249@drexel.edu>
2026-02-04 21:07:20 -0500

    auto update

---
+++ app.js
@@ -0,0 +1,19 @@
+gallery = ""
+
+const gallery = images
+    .map(image => `<img src="${image}" width="200" height="200">`)
+    .join("\n");
+
+const fs = require('fs');
+const readmePath = './README.md';
+const startMarker = '';
+const endMarker = '';
+
+const currentContent = fs.readFileSync(readmePath, 'utf8');
+
+const newGalleryHTML = gallery 
+
+const updatedContent = currentContent.replace(
+    new RegExp(`${startMarker}[\\s\\S]*${endMarker}`),
+    `${startMarker}\n${newGalleryHTML}\n${endMarker}`
+);
